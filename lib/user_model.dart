6cd339ce6eca55b253f609ae5f841006c8ea4613
Adebowale Damilare <damidee96@gmail.com>
2024-08-17 15:18:19 +0100

    Update user_model.dart

--- lib/user_model.dart
+++ lib/user_model.dart
@@ -1,1 +1,15 @@
-// TODO Implement this library.
+class User {
+  final int id;
+  final String name;
+  final String email;
+
+  User({required this.id, required this.name, required this.email});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      name: json['name'],
+      email: json['email'],
+    );
+  }
+}
