09f47e29b0a0b783c5d3eec2f6fdbd4492497ed9
DamilareAde <damidee96@gmail.com>
2024-08-17 09:06:11 +0100

    Add all project files

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,1 +1,19 @@
-// TODO Implement this library.
+
+import 'package:http/http.dart' as http;
+import 'user_model.dart';
+
+class ApiService {
+  static const String apiUrl = 'http://localhost:3000/api/users';
+
+  Future<List<User>> fetchUsers() async {
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      var json;
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((user) => User.fromJson(user)).toList();
+    } else {
+      throw Exception('Failed to load users');
+    }
+  }
+}
